6095f8c5881e0517024d5d900827177adfafb143
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2026-03-15 18:11:33 +0530

    config change after deploy

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://recoil.yashpz.in',
+  // baseURL: 'http://localhost:3000',
 })
